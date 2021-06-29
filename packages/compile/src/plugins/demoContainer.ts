61e514d75c2fd391d2e380cdd8b43a6177f7889d
poyoho <907415276@qq.com>
2021-06-29 21:43:02 +0800

    feat(compile): :construction: make the description tokens render once

--- packages/compile/src/plugins/demoContainer.ts
+++ packages/compile/src/plugins/demoContainer.ts
@@ -3,36 +3,36 @@ import Token from "markdown-it/lib/token"
 import MarkdownItContainer from "markdown-it-container"
 
 export function DemoContainer(md: MarkdownIt, supportLanguage: string[] = ["vue", "react"]) {
-  // 使用md插件,让md支持 :::demo 语法
+  md.renderer.rules.demo_container_block = () => ""
+  // :::demo container
   md.use(MarkdownItContainer, "demo", {
     render(tokens: Token[], idx: number) {
       if (tokens[idx].nesting === 1) {
         const currentTokens = tokens.slice(idx+1, tokens.findIndex(token => token.type === "container_demo_close"))
-        const groupedToken = currentTokens.reduce((prev, token) => {
-          if(token.type === "fence") {
-            const info = token.info.trim()
-            if (supportLanguage.includes(info)) {
-              prev.fence.push(token)
-            } else {
-              prev.desc.push(token)
-            }
+
+        const descTokens = [] as Token[]
+        let fence = null as Token | null
+        for(const token of currentTokens) {
+          if (
+            token.type === "fence"
+            && !fence
+            && supportLanguage.includes(token.info.trim())
+          ) {
+            fence = token
           } else {
-            prev.desc.push(token)
+            descTokens.push(token)
           }
-          return prev
-        }, {
-          fence: [] as Token[],
-          desc: [] as Token[]
-        })
-        // TODO delete rendered tokens
-        console.log("DemoContainer")
+        }
+
         let content = ""
-        if (groupedToken.fence.length !== 0) {
-          const token = groupedToken.fence[0]
+        if (fence) {
+          const token = fence
           content = token.content
           token.info += " component" // highlight using fence
         }
-        const desc = md.renderer.render(groupedToken.desc, md.options, {})
+        const desc = md.renderer.render(descTokens, md.options, {})
+        // after render make the descript tokens don't render again
+        descTokens.forEach(token => (token.type = "demo_container_block"))
         return [
           `<demo-block>`,
           ` ${desc ? `<div slot="description">${desc}</div>` : ""}`,
