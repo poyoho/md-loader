8e2ab243f51176fe463d0230576e6c27e5d5da7b
poyoho <907415276@qq.com>
2021-06-29 22:19:05 +0800

    feat(component): :sparkles: TODO add control mutation and more feature

--- packages/components/src/demo-block/demo-block-element.ts
+++ packages/components/src/demo-block/demo-block-element.ts
@@ -3,6 +3,29 @@ function html(...args: any) {
 }
 
 export default html`
+<div class="demo-block">
+  <!-- 代码执行ui -->
+  <div class="exec">
+    <slot name="exec"></slot>
+  </div>
+  <!-- 源码区 -->
+  <div class="expaned-block source">
+    <div class="description">
+      <slot name="description"></slot>
+    </div>
+    <div class="highlight">
+      <slot name="highlight"></slot>
+    </div>
+  </div>
+  <!-- 控制区 -->
+  <div class="control">
+    <!-- TODO add control mutation and more feature -->
+    <em class="icon copy">copy</em>
+    <em class="icon codepen">codepen</em>
+    <em class="icon isExpand"></em>
+    <span>展开</span>
+  </div>
+</div>
 <style>
   .demo-block{
     border: 1px solid #ebebeb;
@@ -46,6 +69,9 @@ export default html`
     font-size: 14px;
     color: #e5e7eb;
   }
+  .control .icon:hover {
+    color: red
+  }
   .control:hover {
     color: #409eff
   }
@@ -54,24 +80,4 @@ export default html`
     padding-left: 20px;
   }
 </style>
-<div class="demo-block">
-  <!-- 代码执行ui -->
-  <div class="exec">
-    <slot name="exec"></slot>
-  </div>
-  <!-- 源码区 -->
-  <div class="expaned-block source">
-    <div class="description">
-      <slot name="description"></slot>
-    </div>
-    <div class="highlight">
-      <slot name="highlight"></slot>
-    </div>
-  </div>
-  <!-- 控制区 -->
-  <div class="control">
-    <i class="hovering icon"></i>
-    <span class="hovering">tip text</span>
-  </div>
-</div>
 `
